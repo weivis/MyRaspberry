d0ac2b77521357c4d3de941e756a3ffba4628fe3
ShenVi <yvban@outlook.com>
2019-12-31 16:17:25 +0800

    no message

--- opencamera.py
+++ opencamera.py
@@ -1,14 +1,30 @@
-# -*- coding: UTF-8 -*-
 import cv2
-#引入库
-cap = cv2.VideoCapture(0)
-while True:
-    ret, frame = cap.read()
-    cv2.imshow("Video", frame)
-#读取内容
-    if cv2.waitKey(10) == ord("q"):
-        break
-        
-#随时准备按q退出
-cap.release()
-cv2.destroyAllWindows()
+import os
+ 
+def generate():
+    # path = os.path.abspath('E:/MySMP/haarcascades/haarcascade_frontalface_default.xml')
+    path = os.path.abspath('haarcascades/haarcascade_frontalface_default.xml')
+    print(path)
+    face_cascade = cv2.CascadeClassifier(path)
+
+    camera = cv2.VideoCapture(0)
+    count = 0
+    while (True):
+        ret, frame = camera.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x,y,w,h) in faces:
+            img = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+            f = cv2.resize(gray[y:y+h, x:x+w], (200, 200))
+ 
+        cv2.imshow("camera", frame)
+        if cv2.waitKey(100) & 0xff == ord("q"):
+            break
+        elif count > 1:
+            break
+ 
+    camera.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    generate()
